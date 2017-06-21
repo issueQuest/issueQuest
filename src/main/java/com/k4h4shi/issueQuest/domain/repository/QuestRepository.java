8cba8acac6c60e899a08d0ad59dce7eca09414f2
kotaro takahashi <kotaro.t@k4h4shi.com>
2017-06-21 22:25:06 +0900

    不要なコメントを除去

--- src/main/java/com/k4h4shi/issueQuest/domain/repository/QuestRepository.java
+++ src/main/java/com/k4h4shi/issueQuest/domain/repository/QuestRepository.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.k4h4shi.issueQuest.domain.repository;
 
 import com.k4h4shi.issueQuest.domain.model.Quest;
@@ -9,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * 
  * クエストのリポジトリ
+ * 
  * @author k4h4shi
  *
  */
-public interface QuestRepository extends JpaRepository<Quest, String> {}
+public interface QuestRepository extends JpaRepository<Quest, String> {
+}
